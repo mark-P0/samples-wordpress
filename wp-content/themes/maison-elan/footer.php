b1b63d7786567fbcbdc123a2ad4b980fb166f550
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2026-09-01 10:25:25 +0800

    Link footer to pricing and studio pages

--- wp-content/themes/maison-elan/footer.php
+++ wp-content/themes/maison-elan/footer.php
@@ -5,8 +5,8 @@
       <a href="<?php echo esc_url(home_url('/')); ?>">Home</a>
       <a href="<?php echo esc_url(elan_theme_services_url()); ?>">Services</a>
       <a href="<?php echo esc_url(elan_theme_specialists_url()); ?>">Specialists</a>
-      <a href="<?php echo esc_url(home_url('/#pricing')); ?>">Pricing</a>
-      <a href="<?php echo esc_url(home_url('/#studio')); ?>">Studio</a>
+      <a href="<?php echo esc_url(elan_theme_pricing_url()); ?>">Pricing</a>
+      <a href="<?php echo esc_url(elan_theme_studio_url()); ?>">Studio</a>
       <a href="<?php echo esc_url(elan_contact_url()); ?>">Contact</a>
     </nav>
     <div class="elan-footer__meta">© <?php echo esc_html(date('Y')); ?> Maison Élan</div>
