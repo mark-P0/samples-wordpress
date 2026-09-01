49820e8d1656cec84d942f777ad781b6856cd0c5
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2026-09-01 09:56:12 +0800

    Use theme route helpers in footer

--- wp-content/themes/maison-elan/footer.php
+++ wp-content/themes/maison-elan/footer.php
@@ -3,8 +3,8 @@
     <a class="elan-brand" href="<?php echo esc_url(home_url('/')); ?>">Maison Élan</a>
     <nav class="elan-footer__nav" aria-label="Footer navigation">
       <a href="<?php echo esc_url(home_url('/')); ?>">Home</a>
-      <a href="<?php echo esc_url(elan_services_url()); ?>">Services</a>
-      <a href="<?php echo esc_url(function_exists('elan_specialists_url') ? elan_specialists_url() : home_url('/#specialists')); ?>">Specialists</a>
+      <a href="<?php echo esc_url(elan_theme_services_url()); ?>">Services</a>
+      <a href="<?php echo esc_url(elan_theme_specialists_url()); ?>">Specialists</a>
       <a href="<?php echo esc_url(home_url('/#pricing')); ?>">Pricing</a>
       <a href="<?php echo esc_url(home_url('/#studio')); ?>">Studio</a>
       <a href="<?php echo esc_url(elan_contact_url()); ?>">Contact</a>
